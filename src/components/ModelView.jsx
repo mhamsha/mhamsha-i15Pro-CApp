561e39a25e1603d6a577c765b05226cbb0ac4869
mhamsha <hamza.shahzad.8692@gmail.com>
2024-08-09 13:09:29 +0500

    chore: Comment out IPhone component in ModelView.jsx

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -68,12 +68,12 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
           />
           <group ref={groupRef} name={`${index === 1 ? "small" : "large"}`} position={[0, 0, 0]}>
             <Suspense fallback={<Loader />}>
-              <IPhone
+              {/* <IPhone
                 key={size}
                 scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
                 item={item}
                 size={size}
-              />
+              /> */}
             </Suspense>
           </group>
         </View>
